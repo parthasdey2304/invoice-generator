9a5a559ecd0befe2442c85a5cf6a9b4fed431032
Partha <parthasdeyofficial@gmail.com>
2024-07-15 14:44:18 +0530

    initial commit

--- src/components/InvoiceForm.jsx
+++ src/components/InvoiceForm.jsx
@@ -53,7 +53,7 @@ const InvoiceForm = ({ onSubmit }) => {
 
   return (
     <div className='p-2'>
-    <form onSubmit={handleSubmit} className="p-6 mx-auto max-w-4xl bg-white border-blue-300 border-2 shadow-lg rounded-lg py-10 my-10">
+    <form onSubmit={handleSubmit} className="p-6 mx-auto max-w-4xl bg-white border-blue-300 border-2 shadow-lg rounded-lg py-10 md:my-10">
       <h2 className="text-3xl font-bold mb-6 text-gray-800">Invoice Details</h2>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
         <div>
