e18d36e3a356f505845ef520c37f2656308aff3b
Partha <parthasdeyofficial@gmail.com>
2024-10-27 13:11:56 +0530

    Update : UI got updated!

--- generator/src/components/InvoiceForm.jsx
+++ generator/src/components/InvoiceForm.jsx
@@ -30,7 +30,7 @@ const InvoiceForm = ({ onSubmit }) => {
     pdfLink: ''
   });
 
-  const [isDarkTheme, setIsDarkTheme] = useState(false);
+  const [isDarkTheme, setIsDarkTheme] = useState(true);
   const [showMessage, setShowMessage] = useState(true);
 
   useEffect(() => {
@@ -73,20 +73,22 @@ const InvoiceForm = ({ onSubmit }) => {
     <div className={`p-2 transition-colors duration-300 ${isDarkTheme ? 'dark:bg-gray-900' : 'bg-white'}`}>
       <div className="fixed top-4 right-4 flex items-center">
         {showMessage && (
-          <span className="mr-4 text-xl font-['Poppins'] font-medium text-gray-700 dark:text-gray-700 animate-bounce">
-            Try our new dark mode toggle!
+          <span className="mr-4 text-xl font-['Poppins'] font-semibold text-gray-700 dark:text-gray-300 animate-bounce p-1 rounded-lg bg-gradient-to-r from-red-500 via-yellow-500 via-green-500 via-blue-500 to-purple-500">
+            <span className="bg-white dark:bg-gray-800 px-2 py-1 rounded">
+              Try our new dark mode toggle!
+            </span>
           </span>
         )}
         <label className="inline-flex items-center cursor-pointer">
           <input type="checkbox" value="" className="sr-only peer" checked={isDarkTheme} onChange={toggleTheme} />
           <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
-          <span className="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">
+          <span className="-mt-1 ml-3 text-md font-medium text-gray-900 dark:text-gray-300">
             {isDarkTheme ? '🌙' : '☀️'}
           </span>
         </label>
       </div>
 
-      <h1 className={`text-center text-5xl font-bold py-10 ${isDarkTheme ? 'text-white' : 'text-gray-800'}`}>
+      <h1 className={`text-center text-5xl font-bold pt-16 py-10 ${isDarkTheme ? 'text-white' : 'text-gray-800'}`}>
         Invoice Generator
       </h1>
 
